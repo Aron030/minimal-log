4daa8c3c1a35884744116722a50587da8f9085d0
zhaochunlong <1960528539@qq.com>
2022-12-07 16:10:42 +0800

    第一次提交

---
+++ src/main/java/com/aron/minimallog/MinimalLogApplication.java
@@ -0,0 +1,13 @@
+package com.aron.minimallog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinimalLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aron/minimallog/MinimalLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aron.minimallog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
